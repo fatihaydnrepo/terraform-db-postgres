82b5f383b00bd6d358d855bfa06225fc8e4beb8c
Fatih AYDIN <128099981+fatihaydnrepo@users.noreply.github.com>
2023-04-25 04:13:14 +0300

    Add files via upload

---
+++ terraform-rds/main.tf
@@ -0,0 +1,77 @@
+
+provider "aws" {
+  region = var.region
+  access_key               = "AKIARZ5UNZREH6TVKSHR"
+  secret_key               = "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"
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "fatih-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "education" {
+  name       = "fatih-subnet"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "fatih"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "education_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "education" {
+  identifier             = "education"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.1"
+  username               = "fatihdenemefatih"
+  password               = "denemefatihdeneme"
+  db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
